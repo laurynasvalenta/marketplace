a4be258aa6f7fcc44329c5cee507c66c2c991349
Laurynas Valenta <valenta.laurynas@gmail.com>
2020-12-07 18:37:32 +0200

    Configure PHP container.

---
+++ order-service/tools/init.sh
@@ -0,0 +1,5 @@
+composer install
+
+bin/console doctrine:database:drop --force
+bin/console doctrine:database:create
+bin/console doctrine:schema:create
---
+++ product-service/tools/init.sh
@@ -0,0 +1,6 @@
+composer install
+
+bin/console doctrine:database:drop --force
+bin/console doctrine:database:create
+bin/console doctrine:schema:create
+
